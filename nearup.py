fc340d58411c79989f1504e313105f8b78bafedb
Bo Yao <bo@nearprotocol.com>
2020-03-10 18:05:15 -0700

    delegate to call main.py

--- nearup.py
+++ nearup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import os
+import subprocess
 import sys
 import platform
 
@@ -9,4 +11,15 @@ if major_version < 3 or minor_version < 6:
     sys.stderr.write('Require Python 3.6 or higher to run nearup\n')
     exit(1)
 
-print('NEARUP')
+main_script = os.path.expanduser('~/.nearup/src/main.py')
+if os.path.exists(main_script):
+    args = ['python3', main_script] + sys.argv[1:]
+    subprocess.run(args)
+else:
+    p = subprocess.Popen(
+        ['git', 'clone', 'https://github.com/nearprotocol/nearup', os.path.expanduser('~/.nearup')])
+    _stdout, _stderr = p.communicate()
+    if p.returncode != 0:
+        sys.stderr.write('Failed to obtain nearup source code. Please retry')
+        subprocess.check_output(['rm', '-rf', os.path.expanduser('~/.nearup')])
+        exit(2)
