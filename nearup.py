df89f3f986e9f68667180e466e050610a859bc06
Bo Yao <bo@nearprotocol.com>
2020-03-10 18:11:08 -0700

    clone easier

--- nearup.py
+++ nearup.py
@@ -11,13 +11,13 @@ if major_version < 3 or minor_version < 6:
     sys.stderr.write('Require Python 3.6 or higher to run nearup\n')
     exit(1)
 
-main_script = os.path.expanduser('~/.nearup/src/main.py')
+main_script = os.path.expanduser('~/.nearup/main.py')
 if os.path.exists(main_script):
     args = ['python3', main_script] + sys.argv[1:]
     subprocess.run(args)
 else:
     p = subprocess.Popen(
-        ['git', 'clone', 'https://github.com/nearprotocol/nearup', os.path.expanduser('~/.nearup/src')], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ['git', 'clone', 'https://github.com/nearprotocol/nearup', os.path.expanduser('~/.nearup')], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     _stdout, _stderr = p.communicate()
     if p.returncode != 0:
         sys.stderr.write('Failed to obtain nearup source code. Please retry')
