a1ea5babdc6a6c247f46ab2b6b2d1e7698a9f7ca
Bo Yao <bo@nearprotocol.com>
2020-03-10 17:43:00 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nearup
 Public scripts to launch near devnet, betanet and testnet node
+
+# Usage
+```
+curl -sL up.nearprotocol.com | python3
+```
--- nearup.py
+++ nearup.py
@@ -6,7 +6,7 @@ version = platform.python_version_tuple()
 major_version = int(version[0])
 minor_version = int(version[1])
 if major_version < 3 or minor_version < 6:
-    sys.stderr.write('Require Python 3.6 or higher to run nearup')
+    sys.stderr.write('Require Python 3.6 or higher to run nearup\n')
     exit(1)
 
 print('NEARUP')
