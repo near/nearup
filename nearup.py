1209b9b6de5bed23ecc51b62147affa9954a7836
Bo Yao <bo@nearprotocol.com>
2020-03-12 10:04:20 -0700

    self update

--- nearup.py
+++ nearup.py
@@ -9,10 +9,16 @@ major_version = int(version[0])
 minor_version = int(version[1])
 if major_version < 3 or minor_version < 6:
     sys.stderr.write('Require Python 3.6 or higher to run nearup\n')
-    exit(1)
+    exit(2)
 
+nearup_dir = os.path.expanduser('~/.nearup')
 main_script = os.path.expanduser('~/.nearup/main.py')
 if os.path.exists(main_script):
+    p = subprocess.Popen('cd ' + nearup_dir + ' && git pull',
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    _stdout, _stderr = p.communicate()
+    if p.returncode != 0:
+        sys.stderr.write('Warning: Failed to update nearup.')
     args = ['python3', main_script] + sys.argv[1:]
     subprocess.run(args)
 else:
@@ -20,6 +26,8 @@ else:
         ['git', 'clone', 'https://github.com/nearprotocol/nearup', os.path.expanduser('~/.nearup')], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     _stdout, _stderr = p.communicate()
     if p.returncode != 0:
-        sys.stderr.write('Failed to obtain nearup source code. Please retry')
+        sys.stderr.write('Failed to obtain nearup. Please retry')
         subprocess.check_output(['rm', '-rf', os.path.expanduser('~/.nearup')])
-        exit(2)
+        exit(1)
+    args = ['python3', main_script] + sys.argv[1:]
+    subprocess.run(args)
