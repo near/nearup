c5ab560573f7053a30825ff35d44ba24c1dd9f4c
Bo Yao <bo@nearprotocol.com>
2020-03-13 18:40:40 -0700

    docker join devnet works

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-print('success')
+import argparse
+import sys
+from nearuplib.net_argparser import create_net_argparser
+from nearuplib.nodelib import setup_and_run
+
+
+TELEMETRY_URL = 'https://explorer.nearprotocol.com/api/nodes'
+
+
+class NearupArgParser(object):
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description='Nearup',
+            usage='''nearup <command> [<args>]
+
+Commands are:
+    testnet    Run a testnet node
+    betanet    Run a betanet node
+    devnet     Run a devnet node
+
+Run nearup <command> --help to see help for specific command
+''')
+        parser.add_argument('command', help='Subcommand to run')
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print('Unrecognized command', file=sys.stderr)
+            parser.print_help(sys.stderr)
+            exit(2)
+        self.command = args.command
+        getattr(self, args.command)()
+
+    def testnet(self):
+        parser = create_net_argparser(description='Run a testnet node')
+        self.args = parser.parse_args(sys.argv[2:])
+
+    def betanet(self):
+        parser = create_net_argparser(description='Run a betanet node')
+        self.args = parser.parse_args(sys.argv[2:])
+
+    def devnet(self):
+        parser = create_net_argparser(description='Run a devnet node')
+        self.args = parser.parse_args(sys.argv[2:])
+
+
+if __name__ == '__main__':
+    nearup_arg_parser = NearupArgParser()
+    command, args = nearup_arg_parser.command, nearup_arg_parser.args
+    if command in ['devnet', 'betanet', 'testnet']:
+        if args.local:
+            print("Flag --local deprecated, please use --nodocker")
+        nodocker = args.nodocker or args.local
+        setup_and_run(nodocker, not args.debug, args.image, args.home,
+                      init_flags=[f'--chain-id={command}'],
+                      boot_nodes=args.boot_nodes,
+                      telemetry_url=TELEMETRY_URL,
+                      verbose=args.verbose)
---
+++ nearuplib/net_argparser.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+
+
+def create_net_argparser(*, description):
+    TELEMETRY_URL = 'https://explorer.nearprotocol.com/api/nodes'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local', action='store_true',
+                        help='deprecated: use --nodocker')
+    parser.add_argument('--nodocker', action='store_true',
+                        help='If set, compiles and runs the node on the machine directly (not inside the docker).')
+    parser.add_argument('--debug', action='store_true',
+                        help='If set, compiles local nearcore in debug mode')
+    parser.add_argument('--verbose', action='store_true',
+                        help='If set, prints verbose logs')
+    parser.add_argument('--home', default=os.path.expanduser('~/.near/'),
+                        help='Home path for storing configs, keys and chain data (Default: ~/.near)')
+    parser.add_argument(
+        '--image', default='auto',
+        help='Image to run in docker (default: auto)')
+    parser.add_argument(
+        '--boot-nodes',
+        help='Specify boot nodes to load from (Default: "")')
+    return parser
