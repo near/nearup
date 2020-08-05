ce03d40c253b9a4462626b3ffb0d7b677c9a6c5b
chefsale <sandi@tenzen.io>
2020-08-05 14:27:10 +0200

    Refactored logging and constants in localnet

--- nearuplib/constants.py
+++ nearuplib/constants.py
@@ -1,5 +1,6 @@
 import os
 
 LOGS_FOLDER = os.path.expanduser('~/.nearup/logs')
+LOCALNET_LOGS_FOLDER = os.path.expanduser("~/.nearup/logs/localnet")
 NODE_PID_FILE = os.path.expanduser('~/.nearup/node.pid')
 WATCHER_PID_FILE = os.path.expanduser('~/.nearup/watcher.pid')
