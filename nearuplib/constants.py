5a0aa5fd59088e4331dd784d12dfb0b16631f226
chefsale <sandi@tenzen.io>
2020-08-05 13:52:02 +0200

    Moved pid files paths to constants

--- nearuplib/constants.py
+++ nearuplib/constants.py
@@ -1,3 +1,5 @@
 import os
 
 LOGS_FOLDER = os.path.expanduser('~/.nearup/logs')
+NODE_PID_FILE = os.path.expanduser('~/.nearup/node.pid')
+WATCHER_PID_FILE = os.path.expanduser('~/.nearup/watcher.pid')
