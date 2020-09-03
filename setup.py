32ddc8811fa3bb8b754fe8c52f34ea84a0d418af
chefsale <sandi@tenzen.io>
2020-09-03 16:04:24 +0200

    Bumped docker and setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nearup",  # Replace with your own username
-    version="0.3.8",
+    version="0.3.9",
     author="Near Inc",
     author_email="hello@near.org",
     description="Public scripts to launch near blockchain nodes",
