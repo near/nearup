1a64d1416f9eae9ddf4ae27a5ba759baaca6323f
Nick Miller <nick@near.org>
2020-09-15 13:16:54 -0500

    nodelib: add process default wait timeout and adjust tests

--- nearuplib/constants.py
+++ nearuplib/constants.py
@@ -5,3 +5,4 @@ LOCALNET_LOGS_FOLDER = os.path.expanduser("~/.nearup/logs/localnet")
 NODE_PID_FILE = os.path.expanduser('~/.nearup/node.pid')
 S3_BUCKET = 'build.nearprotocol.com'
 WATCHER_PID_FILE = os.path.expanduser('~/.nearup/watcher.pid')
+DEFAULT_WAIT_TIMEOUT = 30
