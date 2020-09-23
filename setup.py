4efb1ad8f042f11b4197e55512ba5b490db0d638
Nick Miller <ngmiller@users.noreply.github.com>
2020-09-23 08:27:54 -0500

    nearup: single source version from file and add version command (#112)

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("VERSION", "r") as fh:
+    version = fh.read().strip()
+
 setuptools.setup(
     name="nearup",  # Replace with your own username
-    version="0.5.3",
+    version=version,
     author="Near Inc",
     author_email="hello@near.org",
     description="Public scripts to launch near blockchain nodes",
