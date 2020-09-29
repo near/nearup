886de4707c06ccba00b1ac960990169fe43df02a
Sandi Fatic <sandi.fatic@gmail.com>
2020-09-30 01:34:09 +0200

    fix: nearup watcher no home directory bug (#130)

--- nearuplib/watcher.py
+++ nearuplib/watcher.py
@@ -9,7 +9,7 @@ import psutil
 from nearuplib.constants import DEFAULT_WAIT_TIMEOUT, WATCHER_PID_FILE
 
 
-def run_watcher(net, path=os.path.expanduser('~/.local/bin/watcher')):
+def run_watcher(net, path=os.path.expanduser('~/.local/bin/watcher'), home=''):
     logging.info("Starting the nearup watcher...")
 
     if not os.path.exists(path):
@@ -21,7 +21,7 @@ def run_watcher(net, path=os.path.expanduser('~/.local/bin/watcher')):
         )
         sys.exit(1)
 
-    proc = Popen(['python3', path, 'run', net])
+    proc = Popen(['python3', path, 'run', net, home])
 
     with open(WATCHER_PID_FILE, 'w') as watcher_pid_file:
         watcher_pid_file.write(str(proc.pid))
@@ -33,13 +33,15 @@ def stop_watcher(timeout=DEFAULT_WAIT_TIMEOUT):
             with open(WATCHER_PID_FILE) as pid_file:
                 pid = int(pid_file.read())
                 process = psutil.Process(pid)
-                logging.info(f'Stopping near watcher {process.name()} with pid {pid}...')
+                logging.info(
+                    f'Stopping near watcher {process.name()} with pid {pid}...')
 
                 try:
                     process.terminate()
                     process.wait(timeout=timeout)
                 except psutil.TimeoutExpired:
-                    logging.warning('Watcher taking a long time to terminate...')
+                    logging.warning(
+                        'Watcher taking a long time to terminate...')
                     logging.warning('Killing watcher with pid {pid}')
                     process.kill()
 
