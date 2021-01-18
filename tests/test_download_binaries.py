7e1e3a75d17d43a8ad8efb2620711a8a214396d6
chefsale <sandi@tenzen.io>
2021-01-18 23:32:36 +0000

    feat(mac): fix mac support

--- tests/test_download_binaries.py
+++ tests/test_download_binaries.py
@@ -20,7 +20,8 @@ def setup_module(module):  # pylint: disable=W0613
 
 
 def test_download_binaries():
-    download_binaries('betanet', 'Linux')
+    uname = os.uname()[0]
+    download_binaries('betanet', uname)
 
     expected_binary = 'neard'
     path = os.path.join(NEARUP_BINARY_DIR, expected_binary)
