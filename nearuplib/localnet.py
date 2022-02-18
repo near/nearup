52a84baa7640545c02743294867c67e01345d598
Michal Nazarewicz <mina86@mina86.com>
2022-02-18 16:40:40 +0100

    Introduce util.wraptext and make prompt_* functions use it (#206)
    Rather than repeating the same textwrap.fill(textwrap.dedent(...))
    idiom in multiple places, wrap it into util.wraptext function.  It’s
    not exactly equivalent since it ignores all leading white space
    (rather than dedenting common spaces) and treats empty lines as
    paragraph separators with paragraphs filled independently and
    separated by empty lines in output.
    
    Furthermore, make util.prompt_bool_flag and util.prompt_flag functions
    call the wraptext on prompt message so that caller of those functions
    doesn’t have to do it themselves.  For short one-line prompts it
    doesn’t end up making any difference.

--- nearuplib/localnet.py
+++ nearuplib/localnet.py
@@ -1,5 +1,4 @@
 import click
-import textwrap
 import json
 import logging
 import os
@@ -20,11 +19,12 @@ def run(binary_path, home, num_nodes, num_shards, override, verbose=True, intera
             logging.info("Removing old data.")
             rmtree(home)
     elif interactive:
-        print(textwrap.fill(textwrap.dedent("""\
-        Starting localnet NEAR nodes.  This is a test network entirely local to
-        this machine.  Validators and non-validating nodes will be started, and
-        will communicate with each other on localhost, producing blocks on top
-        of a genesis block generated locally.""")))
+        print(util.wraptext('''
+            Starting localnet NEAR nodes.  This is a test network entirely local
+            to this machine.  Validators and non-validating nodes will be
+            started, and will communicate with each other on localhost,
+            producing blocks on top of a genesis block generated locally.
+        '''))
         print()
 
     if not os.path.exists(home):
